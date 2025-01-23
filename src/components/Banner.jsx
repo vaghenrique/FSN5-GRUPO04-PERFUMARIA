3aa9433eda707b4e8c03e6d2fee4a890553b19e5
artreboucas <arthurreboucas123@gmail.com>
2025-01-22 21:44:01 -0300

    Pagina Home Atualizadas

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,22 +1,71 @@
 import React from 'react';
 import './Banner.css';
-import bleudechanel from '../assets/bleudechanel.png';
+import essencial from '../assets/essencial.jpg';
+import lavie from '../assets/lavie.jpg'
+import natural from '../assets/natural.jpg'
 
 const Banner = () => {
   return (
     <section className="banner">
       <div className="banner-text">
-        <h2>Encontre o seu melhor Aroma</h2>
+        <h2>Encontre o seu <br />melhor Aroma</h2>
         <div className="stats">
-          <div><strong>20+</strong> Produtos</div>
-          <div><strong>5+</strong> Marcas</div>
+          <div className="stat-item">
+            <div className="number"><strong>20+</strong></div>
+            <div className="label">Produtos</div>
+          </div>
+          <div className="stat-item">
+            <div className="number"><strong>5+</strong></div>
+            <div className="label">Marcas</div>
+          </div>
         </div>
-      </div>
-      <div className="banner-image">
-        <img src={bleudechanel} alt="Banner" />
       </div>
     </section>
   );
 };
 
-export default Banner;
+const Header = () => {
+  return (
+    <section className="header">
+      <h1>Perfumes Mais Procurados</h1>
+      <p>Escolha entre os Aromas mais procurados do momento!</p>
+      <button className="btn-saiba-mais">Saiba mais &rarr;</button>
+    </section>
+  );
+};
+
+const Products = () => {
+  return (
+    <section className="products">
+      <div className="product-card">
+      <img src={essencial}  alt="Essencial Exclusivo - Natura" />
+        <h2>Essencial Exclusivo - Natura</h2>
+        <p className="price">₱ 1,400.00</p>
+      </div>
+
+      <div className="product-card">
+        <img src={lavie} alt="La Vie est Belle - Lancome" />
+        <h2>La Vie est Belle - Lancome</h2>
+        <p className="price">₱ 900.00</p>
+      </div>
+
+      <div className="product-card">
+        <img src={natural} alt="Ekos Ryo Chuva - Natura" />
+        <h2>Ekos Ryo Chuva - Natura</h2>
+        <p className="price">₱ 3,500.00</p>
+      </div>
+    </section>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="container">
+      <Banner />
+      <Header />
+      <Products />
+    </div>
+  );
+};
+
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,13 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>Casa dos Aromas</h1>
+      <link href="https://fonts.googleapis.com/css2?family=Krona+One&display=swap" rel="stylesheet" />
+      <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
       <ul>
         <li><a href="#home">Home</a></li>
+        <link href="https://fonts.googleapis.com/css2?family=Krona+One&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
         <li><a href="#produtos">Produtos</a></li>
+        <link href="https://fonts.googleapis.com/css2?family=Krona+One&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
       </ul>
       <div className="cart-icon">🛒</div>
     </nav>
