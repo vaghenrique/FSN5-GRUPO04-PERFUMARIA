8f788fdd9326180c1b756d355b9149d42709059b
Leonardo C. Hilário <hilarioleozinho@gmail.com>
2025-01-23 21:07:18 -0300

    nova tentativa

--- src/components/js/Header.js
+++ src/components/js/Header.js
@@ -1,7 +1,23 @@
 //Cabeçalho:
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
+const Nav = styled.nav`
+  background-color: #333;
+  padding: 1rem;
+  ul {
+    display: flex;
+    list-style: none;
+  }
+  li {
+    margin: 0 1rem;
+  }
+  a {
+    color: white;
+    text-decoration: none;
+  }
+`
 const Header = () => {
     return (
         <header>
